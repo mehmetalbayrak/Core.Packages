b39cd83c50ccbd951ba5f185c3ce466cd3816aec
mehmetalbayrak <mehmet__albayrak@outlook.com>
2024-03-20 13:11:31 +0300

    added user operations models

---
+++ Core.CrossCuttingConcerns/Serilog/ConfigurationModels/MsSqlConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+
+public class MsSqlConfiguration
+{
+    public string ConnectionString { get; set; }
+    public string Table { get; set; }
+    public bool AutoCreateSqlTable { get; set; }
+    public MsSqlConfiguration()
+    {
+        ConnectionString = string.Empty;
+        Table = string.Empty;
+    }
+    public MsSqlConfiguration(string connectionString, string table, bool autoCreateSqlTable)
+    {
+        ConnectionString = connectionString;
+        Table = table;
+        AutoCreateSqlTable = autoCreateSqlTable;
+    }
+}
---
+++ Core.CrossCuttingConcerns/Serilog/Logger/MsSqlLogger.cs
@@ -0,0 +1,32 @@
+﻿using Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Serilog.Logger;
+
+public class MsSqlLogger : LoggerServiceBase
+{
+    public MsSqlLogger(IConfiguration configuration)
+    {
+        MsSqlConfiguration logConfig = configuration.GetSection("MsSqlLogConfigurations:MsSqlLogConfiguration").Get<MsSqlConfiguration>() ?? throw new Exception(SerilogMessages.NullOptionsMessage);
+
+        MSSqlServerSinkOptions sinkOptions = new()
+        {
+            TableName = logConfig.Table,
+            AutoCreateSqlDatabase = logConfig.AutoCreateSqlTable
+        };
+
+        ColumnOptions columnOptions = new();
+        global::Serilog.Core.Logger seriLogConfig = new LoggerConfiguration().WriteTo
+            .MSSqlServer(logConfig.ConnectionString, sinkOptions, columnOptions: columnOptions).CreateLogger();
+
+        Logger = seriLogConfig;
+    }
+}
